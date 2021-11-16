58b8d1f0fd23249fbd4e90ddf003fa56829f564b
zkajo <zkajota@gmail.com>
2021-11-16 09:43:53 +0000

    building helper update to support cultures listed in a file

--- Solution/UIHelper/Program.cs
+++ Solution/UIHelper/Program.cs
@@ -10,16 +10,6 @@ namespace BuildingNamer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Before starting, make sure the files you want to rename are " +
-                "\nlocated in \"Data\\Base\" folder. For example \"#CULTUREWORD_buildingName.tga\"");
-            Console.WriteLine();
-            Console.WriteLine("The program will take anything located under the base directory," +
-                "\nand rename it with appropraite tags, like barbarian, carthagianian and roman. " +
-                "\nThese will be located in the Output folder, where you can delete as needed");
-
-            Console.WriteLine();
-            Console.WriteLine("Anything with a keyword \"CULTUREWORD\" is going to be replaced with a culture tag.");
-            Console.ReadLine();
             App app = new App();
         }
     }
@@ -28,12 +18,46 @@ namespace BuildingNamer
     {
         public App()
         {
+            Console.WriteLine("1: BuildingMaker");
+            Console.WriteLine("2: UnitMaker");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                BuildingMaker maker = new BuildingMaker();
+                maker.Start();
+            }
+        }
+
+
+    }
+
+    public class BuildingMaker
+    {
+        public void Start()
+        {
+            Console.WriteLine("Before starting, make sure the files you want to rename are " +
+                "\nlocated in \"Data\\BuildingMaker\\Base\" folder. For example \"#CULTUREWORD_buildingName.tga\"");
+            Console.WriteLine();
+            Console.WriteLine("The program will take anything located under the base directory," +
+                "\nand rename it with appropraite tags, like barbarian, carthagianian and roman. " +
+                "\nThese will be located in the Output folder, where you can delete as needed");
+
+            Console.WriteLine();
+            Console.WriteLine("Anything with a keyword \"CULTUREWORD\" is going to be replaced with a culture tag.");
+            Console.ReadLine();
+
             Run();
         }
 
         private void Run()
         {
-            string buildingsPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\base");
+            string buildingMakerPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\BuildingMaker");
+            
+            string culturesPath = Path.Combine(buildingMakerPath, "CulturesList.txt");
+            List<string> cultures = File.ReadAllLines(culturesPath).ToList();
+
+            string buildingsPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Data\BuildingMaker\base");
             List<string> fileInfo = Directory.GetFiles(buildingsPath, "*.*", SearchOption.AllDirectories).ToList();
 
             string outputPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"Output");
@@ -46,12 +70,10 @@ namespace BuildingNamer
             {
                 FileInfo info = new FileInfo(file);
 
-                MoveToDirectory(info, "barbarian");
-                MoveToDirectory(info, "carthaginian");
-                MoveToDirectory(info, "eastern");
-                MoveToDirectory(info, "egyptian");
-                MoveToDirectory(info, "greek");
-                MoveToDirectory(info, "roman");
+                foreach (var culture in cultures)
+                {
+                    MoveToDirectory(info, culture);
+                }
             }
         }
 
@@ -59,19 +81,19 @@ namespace BuildingNamer
         {
             string directory = info.Directory.FullName;
 
-            string pathName = info.FullName.Replace(@"Data\base", @"Output\" + culture);
+            string pathName = info.FullName.Replace(@"Data\BuildingMaker\base", @"Output\" + culture);
             string modifiedName = info.Name.Replace("CULTUREWORD", culture);
             pathName = pathName.Replace(info.Name, modifiedName);
 
-            string newDirectory = directory.Replace(@"Data\base", @"Output\" + culture);
+            string newDirectory = directory.Replace(@"Data\BuildingMaker\base", @"Output\" + culture);
             if (!Directory.Exists(newDirectory))
             {
                 Directory.CreateDirectory(newDirectory);
             }
 
-            string display = pathName.Split(new string[] { "Output" }, StringSplitOptions.None)[1];
-            Console.WriteLine("creating: " + display);
+            Console.WriteLine("creating: " + pathName);
             File.Copy(info.FullName, pathName, true);
         }
     }
 }
+
